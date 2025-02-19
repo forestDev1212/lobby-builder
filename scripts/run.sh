03504401d6091f9eedccaaf27038af3c5d61065b
forestDev1212 <alexeidorman@gmail.com>
2025-02-19 22:27:59 +0530

    add: run script for docker entry point

---
+++ scripts/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Navigate to the script's directory
+#
+echo "Reading wallet and safe address from agents folder"
+# wallet ovt key will be present inside the /agent_key/ethereum_private_key.txt folder
+# safe address will be present inside the /agent_key/safe_address.txt folder
+export AGENT_EOA_PK=$(cat /agent_key/ethereum_private_key.txt)
+
+# Create a .env file and read the global ebv vars start with CONNECTION_CONFIGS_CONFIG_
+echo "Creating .env file with CONNECTION_CONFIGS_CONFIG_ variables"
+env | grep '^CONNECTION_CONFIGS_CONFIG_' > .env
+
+echo "Running Agents fun."
+# Run pnpm start
+pnpm start
+
+
+echo "Agent Started."
